9394a7760668afec02a5cb838a76a71789e4db0a
abdulwahabaljimaz-410 <bo3abii5@gmail.com>
2020-06-27 20:29:54 +0300

    4

--- Development/js/scripts.js
+++ Development/js/scripts.js
@@ -1,34 +1,37 @@
 // السؤال الأول
 // 1
-
+let name="wahab";
 // 2
-
+let age=17;
 // 3 and 4
-
+console.log("iam "+name+"  iam"+age);
 // 5 and 6
-
+console.log(age*=5);
 // -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-
 
 // السؤال الثاني
 // 1
-
+const hobby=["football","basketball","boxing"];
 // 2
-
+hobby.push(age);
 // 3
-
+hobby.pop([2]);
 // 4
+let coded={
+    a:"web",b:"hussain" 
+};
 
 // 5
-
+let ta=["nancy", "hussain", "rahaf"];
 // 6
-
+console.log(coded.a);
 // 7
-
+console.log(coded.b);
 // -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-
 
 // البونص
 // 1
-
+let any=["hhhhhhhhhhhh"];
 // 2
-
+console.log(any);
 // console results => console.png
